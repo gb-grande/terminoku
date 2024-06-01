243f2649ea5cb6686beb40d9dc65d7504eed8d60
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2024-06-01 00:44:38 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn num_to_diff(num : i8) -> String {
+    match num {
+        0 => String::from("Easy"),
+        1 => String::from("Medium"),
+        2 => String::from("Hard"),
+        3 => String::from("Very Hard"),
+        _ => String::from("Unknown")
+    }
+}
+
+fn main() {
+    println!("Welcome to Terminoku!");
+    println!("Please choose a difficulty");
+    let mut difficulty : i8 = -1;
+    let mut diff_input = String::new();
+    while difficulty < 0 || difficulty > 3  {
+        println!("0 - Easy\n1 - Medium\n2 - Hard\n3 - Very Hard");
+        io::stdin()
+            .read_line(&mut diff_input)
+            .expect("Failed to read input.");
+        difficulty = diff_input.trim().parse().expect("Input not an integer");
+    }
+    let cd = num_to_diff(difficulty);
+    println!("Chosen difficulty is {cd}");
+}
